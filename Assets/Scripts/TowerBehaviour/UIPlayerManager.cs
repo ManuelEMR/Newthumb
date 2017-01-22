466e8bc2f77d49cffb6d33d4994c2f9950d5ad39
Manuel Munoz <manuelmunozr.92@gmail.com>
2017-01-22 11:11:55 -0400

    :star: Added UIPlayerManager to show which player won ui

---
+++ Assets/Scripts/TowerBehaviour/UIPlayerManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UIPlayerManager : MonoBehaviour {
+
+	public GameObject playerOneWon;
+	public GameObject playerTwoWon;
+
+	Image _image;
+
+	// Use this for initialization
+	void Start () {
+
+		_image = GetComponent<Image> ();
+
+		TowerHealth.OnTowerDestroyed += OnPlayerWon;
+	}
+
+	void OnDestroy(){
+
+		TowerHealth.OnTowerDestroyed -= OnPlayerWon;
+	}
+	
+	void OnPlayerWon(int playerNumber){
+
+		_image.color = _image.color.NewColor (1);
+
+		if (playerNumber == 2)
+			playerOneWon.SetActive (true);
+		else
+			playerTwoWon.SetActive (true);
+
+		this.InvokeAfterSeconds (2, () => {
+
+			//Return to Home scene
+			SceneManager.LoadScene(0);
+		});
+	}
+}
